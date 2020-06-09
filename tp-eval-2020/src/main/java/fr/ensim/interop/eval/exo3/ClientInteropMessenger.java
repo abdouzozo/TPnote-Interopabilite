dc3311b75bcdf7e19693d2e1a7132657733c5437
Abdou <abdouzozo.14@gmail.com>
2020-06-09 16:52:31 +0200

    Ajout boucle 5 sec

--- tp-eval-2020/src/main/java/fr/ensim/interop/eval/exo3/ClientInteropMessenger.java
+++ tp-eval-2020/src/main/java/fr/ensim/interop/eval/exo3/ClientInteropMessenger.java
@@ -21,7 +21,7 @@ public class ClientInteropMessenger {
     // TODO: Exercice 3 - Implémentation d'un client du service Interop Messenger (si langage de programmation Java).
 	//Token : 4ROm3hk8 with email : abderrahman.azhari.etu@univ-lemans.fr
 	
-	public static void main(String[] args) throws URISyntaxException {
+	public static void main(String[] args) throws URISyntaxException, InterruptedException {
 		
 		RestTemplate restTemplate = new RestTemplate();
 		
@@ -43,33 +43,40 @@ public class ClientInteropMessenger {
 		//ResponseEntity<String> result = restTemplate.postForEntity(uri, plantation_1, String.class);
 		final String baseUrl = "https://ensim.flox.dev/channels/AbdouDavidChannel/messages";
 		 URI uri = new URI(baseUrl);
-		 /*
-		 Date now = new Date();
-		 Message message_1 = new Message("Hello Cownard ! ",now.toString(),"abderrahman.azhari.etu@univ-lemans.fr");
-		ResponseEntity<String> result = restTemplate.postForEntity(uri, message_1, String.class);
 		 
-		 //Verify request succeed
-		    System.out.println("le code retour est : "+ result.getStatusCodeValue());
-		 
-		    
-		  //Envoi d'un message saisi sur un canal fixe au lancement du programme
-		    
-		  Scanner sc = new Scanner(System.in);
-		  System.out.println("Veuillez saisir votre message :");
-		  String str_message = sc.nextLine();
-		  now = new Date();
-		  Message message_2 = new Message(str_message,now.toString(),"abderrahman.azhari.etu@univ-lemans.fr");
-		  
-		  result = restTemplate.postForEntity(uri, message_2, String.class);
-			 
-			 //Verify request succeed
-			    System.out.println("le code retour est : "+ result.getStatusCodeValue());
-			*/
-		 
+//		 Date now = new Date();
+//		 Message message_1 = new Message("Hello Cownard ! ",now.toString(),"abderrahman.azhari.etu@univ-lemans.fr");
+//		ResponseEntity<String> result = restTemplate.postForEntity(uri, message_1, String.class);
+//		 
+//		 //Verify request succeed
+//		    System.out.println("le code retour est : "+ result.getStatusCodeValue());
+//		 
+//		    
+//		  //Envoi d'un message saisi sur un canal fixe au lancement du programme
+//		    
+//		  Scanner sc = new Scanner(System.in);
+//		  System.out.println("Veuillez saisir votre message :");
+//		  String str_message = sc.nextLine();
+//		  now = new Date();
+//		  Message message_2 = new Message(str_message,now.toString(),"abderrahman.azhari.etu@univ-lemans.fr");
+//		  
+//		  result = restTemplate.postForEntity(uri, message_2, String.class);
+//			 
+//			 //Verify request succeed
+//			    System.out.println("le code retour est : "+ result.getStatusCodeValue());
+//			
+//		 
 		 //Reception des messages fixes sur un canal fixe au lancement du programme
+			    
+			    
 		 
-		 ResponseEntity<Message[]> response =
-					restTemplate.getForEntity(
+		 ResponseEntity<Message[]> response;
+		 
+		 while(true) {
+			 
+			 Thread.sleep(5000);
+		 
+			 response = restTemplate.getForEntity(
 							"https://ensim.flox.dev/channels/AbdouDavidChannel/messages",
 					 Message[].class);
 					
@@ -78,6 +85,8 @@ public class ClientInteropMessenger {
 				for(Message m : list_Messages) {
 					System.out.println(m.getContent());
 				}
+				
+		 }
 		
 	}
 	
